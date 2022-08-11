5ac656dd1c076cfbfe8b366d2a35d53d263d1a76
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-11 17:04:02 +0100

    chore: make connection info into environment variables

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,7 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: "jonnyabrams",
-  host: "localhost",
-  database: "yulp",
-  port: 5432,
-});
+const pool = new Pool();
 
 export default {
   query: (text, params) => pool.query(text, params),
