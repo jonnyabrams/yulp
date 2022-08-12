b13b74d6addbad86ba3b818e7ce03c3759da964f
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-12 21:15:19 +0100

    feat: backend login functionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,22 +1,18 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
 import db from "../db/index.js";
 
 export const registerUser = async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
     const result = await db.query(
       "INSERT INTO users (username, email, password) values ($1, $2, $3) returning *",
-      [
-        req.body.username,
-        req.body.email,
-        hashedPassword,
-      ]
+      [req.body.username, req.body.email, hashedPassword]
     );
 
-    const { password, ...others } = result.rows[0]
+    const { password, ...others } = result.rows[0];
 
     res.status(201).json({
       status: "success",
@@ -30,7 +26,30 @@ export const registerUser = async (req, res) => {
 };
 
 export const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+
   try {
+    const user = await db.query("select * from users where username = $1", [
+      req.body.username,
+    ]);
+
+    if (user) {
+      const valid = await bcrypt.compare(password, user.rows[0].password);
+
+      if (!valid) {
+        res.status(400).json("Wrong password");
+      } else {
+        const { password, ...others } = user.rows[0];
+        res.status(200).json({
+          status: "success",
+          data: {
+            user: others,
+          },
+        });
+      }
+    } else {
+      res.status(404).json("User does not exist");
+    }
   } catch (error) {
     console.log(error);
   }
