69bf0a3d25be46e04e8e0834e6d92d0cda767d4b
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-12 16:33:19 +0100

    feat: arrange routes in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import RestaurantDetails from "./pages/RestaurantDetails";
+import UpdatePage from "./pages/UpdatePage";
 
 const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+        <Route path="/restaurants/:id" element={<RestaurantDetails />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
