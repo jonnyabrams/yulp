7d905fc329acbe3cddaa2407716938047dcde6d5
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-15 07:19:38 +0100

    feat: restaurants context

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import { AuthContextProvider } from "./context/Auth/AuthContext";
+import { RestaurantsContextProvider } from "./context/Restaurants/RestaurantContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <RestaurantsContextProvider>
+        <App />
+      </RestaurantsContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
