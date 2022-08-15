1c505236a5a83ba03c91d82edaf67ccf5c775083
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-15 08:34:18 +0100

    feat: display restaurant data on table

--- client/src/components/RestaurantList/RestaurantList.jsx
+++ client/src/components/RestaurantList/RestaurantList.jsx
@@ -23,47 +23,24 @@ const RestaurantList = (props) => {
     <table className="table">
       <tbody>
         <tr>
-          <th align="center" className="heading">
-            Restaurant
-          </th>
-          <th align="center" className="heading">
-            Location
-          </th>
-          <th align="center" className="heading">
-            Price Range
-          </th>
-          <th align="center" className="heading">
-            Rating
-          </th>
+          <th className="heading">Restaurant</th>
+          <th className="heading">Location</th>
+          <th className="heading">Price Range</th>
+          <th className="heading">Rating</th>
+          <th className="heading">Edit</th>
+          <th className="heading">Delete</th>
         </tr>
-        <tr>
-          <td align="center" className="restaurant-detail">
-            Pizza Bastard
-          </td>
-          <td align="center" className="restaurant-detail">
-            Bristol
-          </td>
-          <td align="center" className="restaurant-detail">
-            $$
-          </td>
-          <td align="center" className="restaurant-detail">
-            **
-          </td>
-        </tr>
-        <tr>
-          <td align="center" className="restaurant-detail">
-            Sushi Bastard
-          </td>
-          <td align="center" className="restaurant-detail">
-            Leicester
-          </td>
-          <td align="center" className="restaurant-detail">
-            $$$
-          </td>
-          <td align="center" className="restaurant-detail">
-            ****
-          </td>
-        </tr>
+
+        {restaurants.map((restaurant) => (
+          <tr key={restaurant.id}>
+            <td align="center" className="restaurant-detail">{restaurant.name}</td>
+            <td align="center" className="restaurant-detail">{restaurant.location}</td>
+            <td align="center" className="restaurant-detail">{"$".repeat(restaurant.price_range)}</td>
+            <td align="center" className="restaurant-detail">**</td>
+            <td align="center" className="restaurant-detail"><button>Update</button></td>
+            <td align="center" className="restaurant-detail"><button>Delete</button></td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
