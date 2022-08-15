89cfc070296277b407c82738ea2c3a99fde41bb0
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-15 10:11:25 +0100

    feat: addRestaurant frontend functionality

--- client/src/components/AddRestaurant/AddRestaurant.jsx
+++ client/src/components/AddRestaurant/AddRestaurant.jsx
@@ -1,22 +1,58 @@
-import './add-restaurant.css'
+import { useContext, useState } from "react";
+
+import RestaurantFinder from "../../apis/RestaurantFinder";
+import { RestaurantsContext } from "../../context/Restaurants/RestaurantContext";
+import "./add-restaurant.css";
 
 const AddRestaurant = () => {
+  const { addRestaurants } = useContext(RestaurantsContext)
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [priceRange, setPriceRange] = useState("Price range"); // makes "Price range" default on the selector
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await RestaurantFinder.post("/", {
+        name,
+        location,
+        price_range: priceRange
+      });
+      addRestaurants(response.data.data.restaurant)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row">
           <div className="element">
-            <input type="text" placeholder="Name" className="text-input" />
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              placeholder="Name"
+              className="text-input"
+            />
           </div>
           <div className="element">
             <input
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
               type="text"
               placeholder="Location"
               className="text-input"
             />
           </div>
           <div className="element">
-            <select className="dropdown">
+            <select
+              value={priceRange}
+              onChange={(e) => setPriceRange(e.target.value)}
+              className="dropdown"
+            >
               <option disabled>Price range</option>
               <option value="1">$</option>
               <option value="2">$$</option>
@@ -26,7 +62,9 @@ const AddRestaurant = () => {
             </select>
           </div>
           <div className="element">
-            <button className="button">Add</button>
+            <button type="submit" className="button">
+              Add
+            </button>
           </div>
         </div>
       </form>
--- client/src/components/RestaurantList/RestaurantList.jsx
+++ client/src/components/RestaurantList/RestaurantList.jsx
@@ -31,16 +31,29 @@ const RestaurantList = (props) => {
           <th className="heading">Delete</th>
         </tr>
 
-        {restaurants.map((restaurant) => (
-          <tr key={restaurant.id}>
-            <td align="center" className="restaurant-detail">{restaurant.name}</td>
-            <td align="center" className="restaurant-detail">{restaurant.location}</td>
-            <td align="center" className="restaurant-detail">{"$".repeat(restaurant.price_range)}</td>
-            <td align="center" className="restaurant-detail">**</td>
-            <td align="center" className="restaurant-detail"><button>Update</button></td>
-            <td align="center" className="restaurant-detail"><button>Delete</button></td>
-          </tr>
-        ))}
+        {restaurants &&
+          restaurants.map((restaurant) => (
+            <tr key={restaurant.id}>
+              <td align="center" className="restaurant-detail">
+                {restaurant.name}
+              </td>
+              <td align="center" className="restaurant-detail">
+                {restaurant.location}
+              </td>
+              <td align="center" className="restaurant-detail">
+                {"$".repeat(restaurant.price_range)}
+              </td>
+              <td align="center" className="restaurant-detail">
+                **
+              </td>
+              <td align="center" className="restaurant-detail">
+                <button>Update</button>
+              </td>
+              <td align="center" className="restaurant-detail">
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
       </tbody>
     </table>
   );
