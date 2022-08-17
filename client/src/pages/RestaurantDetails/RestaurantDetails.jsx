326dc7e746c4688bfd6600ab39ff43d94c06c830
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-17 08:46:01 +0100

    feat: fetch restaurant data in RestaurantDetails page (with new selectedRestaurant context)

--- client/src/pages/RestaurantDetails/RestaurantDetails.jsx
+++ client/src/pages/RestaurantDetails/RestaurantDetails.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
 
+import { RestaurantsContext } from "../../context/Restaurants/RestaurantContext";
+import RestaurantFinder from "../../apis/RestaurantFinder";
 
 const RestaurantDetails = () => {
-  return (
-    <div>RestaurantDetails</div>
-  )
-}
+  const { id } = useParams();
+  const { selectedRestaurant, setSelectedRestaurant } =
+    useContext(RestaurantsContext);
 
-export default RestaurantDetails
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get(`/${id}`);
+        setSelectedRestaurant(response.data.data.restaurant);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return <div>{selectedRestaurant && selectedRestaurant.name}</div>;
+};
+
+export default RestaurantDetails;
