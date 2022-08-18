06cbde8aa9eb38eb1dc74b3d3d3fe73d1208cbc1
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-18 14:57:47 +0200

    fix: change bigserial to bigint in reviews table to match foreign keys

--- server/db/migrations/03_create_reviews_table.sql
+++ server/db/migrations/03_create_reviews_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE reviews (
   id BIGSERIAL NOT NULL PRIMARY KEY,
-  user_id BIGSERIAL NOT NULL REFERENCES users (id),
-  restaurant_id BIGSERIAL NOT NULL REFERENCES restaurants (id),
+  user_id BIGINT NOT NULL REFERENCES users (id),
+  restaurant_id BIGINT NOT NULL REFERENCES restaurants (id),
   content TEXT NOT NULL,
   rating INT NOT NULL check(rating >=1 and rating <=5)
 );
